394dc33cef5ce3830ab2ca2c81148d61129b8521
doradom <doradombox@outlook.com>
2019-06-29 20:09:04 +0800

    3

--- PrintService/Handler1.ashx.cs
+++ PrintService/Handler1.ashx.cs
@@ -17,7 +17,6 @@ namespace PrintService
     /// </summary>
     public class Handler1 : IHttpHandler
     {
-
         public void ProcessRequest(HttpContext context)
         {
             context.Response.ContentType = "text/html";
